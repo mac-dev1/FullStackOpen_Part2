839dbc0a75e0a0ea9b6165ecf5d51d82aafe5e4c
mac-dev1 <mauriciocasarottodev@outlook.com>
2024-05-09 12:55:17 +0200

    Exercise 2.10 refactored, Exercise 2.11 done

--- The Phonebook/src/App.jsx
+++ The Phonebook/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import Filter from './filter'
-import PersonForm from './personForm'
-import Persons from './persons'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+import Filter from './components/filter'
+import PersonForm from './components/personForm'
+import Persons from './components/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [persons, setPersons] = useState([]) 
   const [filterName, setFilter] = useState('')
+
+  useEffect(()=>{
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(respose=>{
+        console.log('promise fulfilled')
+        console.log(respose.data)
+        setPersons(respose.data)
+      })
+  },[])
 
   return (
     <div>
